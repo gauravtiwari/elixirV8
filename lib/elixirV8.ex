9335db7ec7284241063a3df09fd7346dc545733b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-22 08:58:37 +0300

    Cleanup comments

--- lib/elixirV8.ex
+++ lib/elixirV8.ex
@@ -1,8 +1,6 @@
 defmodule ElixirV8 do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -11,8 +9,6 @@ defmodule ElixirV8 do
       # worker(ElixirV8.Worker, [arg1, arg2, arg3]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ElixirV8.Supervisor]
     Supervisor.start_link(children, opts)
   end
